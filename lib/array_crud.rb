6c0d33a98e623bc348162f2dbd38b07aa68e8c0a
Kasey Arnold <kaseyarnold15@gmail.com>
2019-12-23 03:32:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  1 [1, 2, 3, 4]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  0 [1, 2, 3, 4]
 end
 
 def retrieve_last_element_from_array(array)
